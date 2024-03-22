41693e34981d61e4141570d79d72a720026a89ea
Enzo Soares <enzorodrigues.2003@hotmail.com>
2024-03-22 09:33:38 -0300

    Create CRUD to database

--- src/main/java/br/pucminas/aedsiii/FileUtil/DataBaseAccess.java
+++ src/main/java/br/pucminas/aedsiii/FileUtil/DataBaseAccess.java
@@ -4,54 +4,80 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import main.java.br.pucminas.aedsiii.Entity.Music;
+import main.java.br.pucminas.aedsiii.FileUtil.DTO.MusicDTO;
 
 public class DataBaseAccess {
+	private static char GRAVESTONE_SIGNAL = '*';
 	private RandomAccessFile db;
 	
 	public DataBaseAccess() throws Exception {
 		db = new RandomAccessFile("data.db", "rw");
 	}
 	
-	public void createRecord(Music music) throws IOException {
-		db.seek(0);
-		music.setID(getID());
-		db.seek(db.length());
-		
-		byte[] musicByteArray = music.toByteArray();
-		db.writeChar(' ');
-		db.writeInt(musicByteArray.length);
-		db.write(musicByteArray);
+	public boolean createRecord(Music music) {
+		try {
+			music.setID(getID());
+			db.seek(db.length());
+			
+			byte[] musicByteArray = music.toByteArray();
+			db.writeChar(' ');
+			db.writeInt(musicByteArray.length);
+			db.write(musicByteArray);
+			return true;
+		} catch (Exception e) {
+			System.out.println("Error on create record to: "+music);
+			return false;
+		}
 	}
 	
-	
-	
-	public Music readRecord(int id) throws IOException {
-		db.seek(0);
-		
-		int lastId = !isEmpty() ? db.readInt() : 0;
-		if(id > lastId) {
-			System.out.println("ID not exists");
-			return null;
-		}
-		
-		int size;
-		byte [] recording;
-		boolean EOF = false;
-		Music music = new Music();
+	public Music readRecord(int id) {
+		if(recordCanExists(id)) { return null; }
 
-		while(!EOF) {
-			try {
-				char gravestone = db.readChar();
-				size = db.readInt();
-				recording = new byte[size];
-				db.read(recording);
-				if(gravestone != '*') {
-					music.fromByteArray(recording);
-					if(music.getID() == id) { return music; }
-				}
-			} catch (EOFException e) { EOF = true; }
+		MusicDTO dto = search(id);
+		
+		return dto != null ? dto.getMusic() : null;
+	}
+	
+	public boolean deleteRecord(int id) {
+		if(recordCanExists(id)) { return false; }
+		
+		MusicDTO dto = search(id);
+		if(dto == null) { return false; }
+		
+		try {
+			db.seek(dto.getGravestonePointer());
+			db.writeChar(GRAVESTONE_SIGNAL);
+		} catch(IOException e) {
+			System.out.println("Error on delete record: "+id);
+			return false;
 		}
-		return null;
+		
+		return true;
+	}
+	
+	public boolean updateRecord(Music music) {
+		MusicDTO dto = search(music.getID());
+		if(dto == null) { return false; }
+		
+		try {
+			byte[] newMusic = music.toByteArray();
+			byte[] oldMusic = dto.getMusic().toByteArray();
+			
+			if(newMusic.length <= oldMusic.length) {
+				db.seek(dto.getRecordPointer());
+			} else {
+				db.seek(dto.getGravestonePointer());
+				db.writeChar(GRAVESTONE_SIGNAL);
+				db.seek(db.length());
+			}
+			
+			db.write(newMusic);
+		} catch(IOException e) {
+			System.out.println("Error on update record: "+music);
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public void close() throws IOException {
@@ -61,7 +87,59 @@ public class DataBaseAccess {
 	
 	// MARK: - Private Functions
 
+	private MusicDTO search(int id) {
+		int size;
+		long recordPointer, gravestonePointer;
+		byte [] recording;
+		boolean EOF = false;
+		Music music = new Music();
+
+		while(!EOF) {
+			try {
+				gravestonePointer = db.getFilePointer();
+				char gravestone = db.readChar();
+				
+				size = db.readInt();
+				
+				recordPointer = db.getFilePointer();
+				recording = new byte[size];
+
+				db.read(recording);
+				if(gravestone != GRAVESTONE_SIGNAL) {
+					music.fromByteArray(recording);
+					if(music.getID() == id) { 
+						return new MusicDTO(music, gravestonePointer, recordPointer);
+					}
+				}
+			} catch (EOFException e) { 
+				EOF = true; 
+			} catch(IOException e) {
+				System.out.println("Error on searching music: "+ e);
+				return null;
+			}
+		}
+		
+		return null;
+	}
+	
+	private boolean recordCanExists(int id) {
+		try {
+			db.seek(0);
+			int lastId = !isEmpty() ? db.readInt() : 0;
+			if(id > lastId) {
+				System.out.println("ID cannot exists. ID greater than the last ID.");
+				return false;
+			}
+		} catch(IOException e) {
+			System.out.println("Error on validate ID.");
+			return false;
+		}
+		
+		return true;
+	}
+
 	private int getID() throws IOException {
+		db.seek(0);
 		if(isEmpty()) {
 			db.writeInt(0);
 			return 0;
